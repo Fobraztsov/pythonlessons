399e670f691723bf76673cb592affb675adcb9ef
Fedor Obraztsov <teobraz1983@gmail.com>
2022-02-08 20:25:19 +0300

    add file

--- lesson1.py
+++ lesson1.py
@@ -7,3 +7,11 @@ while True:
         break
     else:
         print('число должно быть > 0 и < 100')
+
+#второе задание
+import time
+
+sec = 64852
+type_result = time.gmtime(sec)
+result = time.strftime("%H:%M:%S",type_result)
+print(result)
