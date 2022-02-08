f3c5218c4f076b31a2608674cd09c672425584ff
Fedor Obraztsov <teobraz1983@gmail.com>
2022-02-08 19:29:06 +0300

    add file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+while True:
+    number = int(input('Введите число: '))
+    if number > 0 and number < 100:
+        print(number*2)
+        print(number / 2)
+        print(number**2)
+        break
+    else:
+        print('число должно быть > 0 и < 100')
 
 
 
